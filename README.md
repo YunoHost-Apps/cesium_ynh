640fa2770a7f3b4250798a9e02769b50cf77df9d
Robles Rodolphe <rodolphe.robles@sfr.fr>
2020-06-03 19:42:02 +0200

    commnet helper-update-version

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Cesium package for YunoHost
 
-[![Integration level](https://dash.yunohost.org/integration/cesium.svg)](https://dash.yunohost.org/appci/app/cesium) ![](https://ci-apps.yunohost.org/ci/badges/cesium.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/cesium.maintain.svg)  
+[![Integration level](https://dash.yunohost.org/integration/cesium.svg)](https://dash.yunohost.org/appci/app/cesium) ![](https://ci-apps.yunohost.org/ci/badges/cesium.status.svg) ![](https://ci-apps.yunohost.org/ci/badges/cesium.maintain.svg)
 [![Install Custom Webapp with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=cesium)
 
 
-> *This package allow you to install Cesium quickly and simply on a YunoHost server.  
+> *This package allow you to install Cesium quickly and simply on a YunoHost server.
 If you don't have YunoHost, please see [here](https://yunohost.org/#/install) to know how to install and enjoy it.*
 
 ## Overview
@@ -12,7 +12,7 @@ If you don't have YunoHost, please see [here](https://yunohost.org/#/install) to
 [Cesium](https://cesium.app) is an [Unhosted webapp](https://unhosted.org) client for any [Duniter](https://duniter.org) crypto-currency.
 It allows you to manage your wallet, certify your friends, and more !
 
-**Shipped version:** v1.6.3
+**Shipped version:** v1.6.7
 
 ## Links
 
--- helper-update-version.sh
+++ helper-update-version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Retrieve sources, get sha256sum, parse package version and update these strings in manifest, README and /conf/app.src
- 
-URL=$(curl -s https://api.github.com/repos/duniter/cesium/releases/latest | grep "browser_" | grep "web" | head -1 | cut -d\" -f4)
-wget -nc --quiet $URL -P ./tmp
-CHECKSUM=$(sha256sum ./tmp/cesium-*-web.zip | head -c 64)
-sed -i "s/SOURCE_SUM=.*/SOURCE_SUM=${CHECKSUM}/" ./conf/app.src
-sed -i "s#SOURCE_URL=.*#SOURCE_URL=$URL#" ./conf/app.src
 
-VERSION=$(echo $URL | sed "s#https://github.com/duniter/cesium/releases/download/\(.*\)/cesium.*#\1#")
-sed -i "s#\*\*Shipped version:\*\*.*#\*\*Shipped version:\*\* ${VERSION}#" ./README.md
-sed -i "s#    \"version\": \".*#    \"version\": \"${VERSION}\~ynh1\",#" ./manifest.json
+# URL=$(curl -s https://api.github.com/repos/duniter/cesium/releases/ | grep "browser_" | grep "web" | head -1 | cut -d\" -f4)
+# wget -nc --quiet $URL -P ./tmp
+# CHECKSUM=$(sha256sum ./tmp/cesium-*-web.zip | head -c 64)
+# sed -i "s/SOURCE_SUM=.*/SOURCE_SUM=${CHECKSUM}/" ./conf/app.src
+# sed -i "s#SOURCE_URL=.*#SOURCE_URL=$URL#" ./conf/app.src
 
-rm -f -R ./tmp
+# VERSION=$(echo $URL | sed "s#https://github.com/duniter/cesium/releases/download/\(.*\)/cesium.*#\1#")
+# sed -i "s#\*\*Shipped version:\*\*.*#\*\*Shipped version:\*\* ${VERSION}#" ./README.md
+# sed -i "s#    \"version\": \".*#    \"version\": \"${VERSION}\~ynh1\",#" ./manifest.json
 
-git commit README.md manifest.json conf/app.src -m "$VERSION"
+# rm -f -R ./tmp
+
+# git commit README.md manifest.json conf/app.src -m "$VERSION"
